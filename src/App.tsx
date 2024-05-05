700e4a373b3d6279344c4d0b32fb757514a84c02
Matus Vanco <matus.vanco@24i.com>
2024-05-05 14:54:31 +0200

    Include variable from published package

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import myurl from "mvanco-react-typescript";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload. I got url {myurl} from my own published Node package.
         </p>
         <a
           className="App-link"
