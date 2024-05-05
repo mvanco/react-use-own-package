315a0fefd4e66c25963375a455b9a8b4f066cdd5
Matus Vanco <matus.vanco@24i.com>
2024-05-05 18:31:29 +0200

    Minor changes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,4 +4,8 @@ function Button({ text, onClick }: { text: string, onClick: () => void }) {
   );
 }
 
+// const Button = ({ text, onClick }: { text: string, onClick: () => void }) => (
+//   <button onClick={onClick}>{text}</button>
+// );
+
 export default Button;
