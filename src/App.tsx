5bdb63b48eab9e4bb238213a27eaaa947b9d5629
Matúš Vančo <Matus.Vanco@nukib.cz>
2024-05-07 14:37:23 +0200

    Connect to Python REST backend

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,49 @@ import "./App.css";
 import myurl from "mvanco-react-typescript";
 import Time from "./components/Time";
 import Button from "./components/Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function App() {
   const [ms, setMs] = useState(0);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('https://matoosh.eu/rest/timer/get_current_time', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.error('Got data: ', data.result);
+      setMs(data.result * 1000);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  const [running, setRunning] = useState(true);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      fetchData();
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [running]);
+
   return (
-    <div className="App">
+    <>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -27,10 +64,45 @@ function App() {
       <body>
         <div className="vertical-linear-layout">
           <Time ms={ms} />
-          <Button text="Start" onClick={() => setMs(ms+1000)} />
+          <Button text={(running) ? "Stop" : "Start"} onClick={() => {
+            try {
+              if (running) {
+                fetch('https://matoosh.eu/rest/timer/stop', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                  }),
+                });
+              } else {
+                fetch('https://matoosh.eu/rest/timer/start', {
+                  method: 'POST',
+                  headers: {
+                    'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify({
+                  }),
+                });
+              }
+            } catch (error) {
+              console.error('Error fetching data:', error);
+            }
+            setRunning(!running);
+          }} />
+          <Button text="Reset" onClick={() => {
+            fetch('https://matoosh.eu/rest/timer/reset', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+              }),
+            });
+          }} />
         </div>
       </body>
-    </div>
+    </>
   );
 }
 
