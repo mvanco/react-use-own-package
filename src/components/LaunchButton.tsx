90f213b489e6b68e97d377c3b12631c1f81089fd
Matus Vanco <matus.vanco@24i.com>
2024-05-10 23:12:46 +0200

    Fix bugs

--- src/components/LaunchButton.tsx
+++ src/components/LaunchButton.tsx
@@ -4,19 +4,19 @@ const LaunchButton = ({ type, onClick }: { type: string, onClick: () => void }) 
   if (type === "stop") {
     return (
       <div className="Button-container">
-        <Button text={"Stop"} style={{ background: "red", width: "30%" }} onClick={onClick} />
+        <Button text={"Stop"} style={{ background: "red", width: "256px" }} onClick={onClick} />
       </div>
     );
   } else if (type === "start") {
     return (
       <div className="Button-container">
-        <Button text={"Start"} style={{ background: "#00aa00ff", width: "30%" }} onClick={onClick} />
+        <Button text={"Start"} style={{ background: "#00aa00ff", width: "256px" }} onClick={onClick} />
       </div>
     );
   } else {
     return (
       <div className="Button-container">
-          <Button text={"Reset"} style={{ background: "grey", width: "30%" }} onClick={onClick} />
+          <Button text={"Reset"} style={{ background: "grey", width: "256px" }} onClick={onClick} />
       </div>
     );
   }
